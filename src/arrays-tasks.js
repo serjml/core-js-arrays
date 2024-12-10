f7a1e896f1b6f2fb002091d0c0f8a16995bc552c
Сергей Ляхов <serjml@Mac-Serjml.local>
2024-12-10 19:15:49 +0300

    feat: solve propagateItemsByPositionIndex & shiftArray task

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -515,8 +515,8 @@ function findLongestIncreasingSubsequence(nums) {
  *  propagateItemsByPositionIndex([ 'a', 'b', 'c', null ]) => [ 'a', 'b', 'b', 'c', 'c', 'c',  null, null, null, null ]
  *  propagateItemsByPositionIndex([ 1,2,3,4,5 ]) => [ 1, 2, 2, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 5 ]
  */
-function propagateItemsByPositionIndex(/* arr */) {
-  throw new Error('Not implemented');
+function propagateItemsByPositionIndex(arr) {
+  return arr.flatMap((el, i) => new Array(i + 1).fill(el));
 }
 
 /**
@@ -532,8 +532,11 @@ function propagateItemsByPositionIndex(/* arr */) {
  *    shiftArray(['a', 'b', 'c', 'd'], -1) => ['b', 'c', 'd', 'a']
  *    shiftArray([10, 20, 30, 40, 50], -3) => [40, 50, 10, 20, 30]
  */
-function shiftArray(/* arr, n */) {
-  throw new Error('Not implemented');
+function shiftArray(arr, n) {
+  if (n < 0) {
+    return arr.slice(-n).concat(arr.slice(0, -n));
+  }
+  return arr.slice(-n).concat(arr.slice(0, n + 1));
 }
 
 /**
