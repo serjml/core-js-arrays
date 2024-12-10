e229fdfdd89a4fc4f1542064a0d6e31031d30a5e
Сергей Ляхов <serjml@Mac-Serjml.local>
2024-12-10 17:11:03 +0300

    feat: solve getElementByIndices & getFalsyValuesCount task

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -366,8 +366,8 @@ function generateOdds(len) {
  *   getElementByIndices(['one','two','three'], [2]) => 'three'  (arr[2])
  *   getElementByIndices([[[ 1, 2, 3]]], [ 0, 0, 1 ]) => 2        (arr[0][0][1])
  */
-function getElementByIndices(/* arr, indices */) {
-  throw new Error('Not implemented');
+function getElementByIndices(arr, indices) {
+  return indices.reduce((array, el) => array[el], arr);
 }
 
 /**
@@ -382,8 +382,8 @@ function getElementByIndices(/* arr, indices */) {
  *  getFalsyValuesCount([ -1, 'false', null, 0 ]) => 2
  *  getFalsyValuesCount([ null, undefined, NaN, false, 0, '' ]) => 6
  */
-function getFalsyValuesCount(/* arr */) {
-  throw new Error('Not implemented');
+function getFalsyValuesCount(arr) {
+  return arr.filter((el) => Boolean(el) === false).length;
 }
 
 /**
